137292783d31330a546294751c1811bf82c546a0
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-10-17 22:20:17 -0500

    Movimiento usando CharacterController y salto con gravedad

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
+    public float gravityScale = 5f;
 
     private Vector3 moveDirection;
+
+    public CharacterController charController;
     
     // Start is called before the first frame update
     void Start()
@@ -18,7 +21,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float yStore = moveDirection.y;
+        
+        // Movimiento
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        transform.position = transform.position + (moveDirection * Time.deltaTime * moveSpeed);
+        moveDirection = moveDirection * moveSpeed;
+        moveDirection.y = yStore;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+
+        charController.Move(moveDirection * Time.deltaTime);
     }
 }
