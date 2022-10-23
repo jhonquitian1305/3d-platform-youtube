2a1c14a5952461e9ef9065c764255cc6c5a0e6d4
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-10-23 12:12:06 -0500

    Arreglando problema de la gravedad

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -43,6 +43,7 @@ public class PlayerController : MonoBehaviour
 
         if(charController.isGrounded)
         {
+            moveDirection.y = 0f;
             if (Input.GetButtonDown("Jump"))
             {
                 moveDirection.y = jumpForce;
