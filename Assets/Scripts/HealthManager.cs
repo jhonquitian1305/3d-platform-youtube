eb5a5b3702d7b50c8b201f48f41772533a0d90a9
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-10-24 21:38:12 -0500

    Configurando sistema de vidas

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public static HealthManager instance;
+
+    public int currentHealth, maxHealth;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Hurt()
+    {
+        currentHealth--;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            GameManager.instance.Respawn();
+        }
+    }
+}
