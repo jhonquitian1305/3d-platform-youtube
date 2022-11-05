af9b353db1cbd5940777a13d3fb2299524e8cb29
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-11-04 20:28:46 -0500

    Efecto al agarrar corazón

--- Assets/Scripts/HealthPickupController.cs
+++ Assets/Scripts/HealthPickupController.cs
@@ -7,11 +7,15 @@ public class HealthPickupController : MonoBehaviour
     public int healthAmount;
     public bool isFullHealth;
 
+    public GameObject healthEffect;
+
     private void OnTriggerEnter(Collider playerCollider)
     {
         if (playerCollider.tag == "Player")
         {
             Destroy(gameObject);
+            
+            Instantiate(healthEffect, PlayerController.instance.transform.position + new Vector3(0f, 1f, 0f), PlayerController.instance.transform.rotation);
 
             if (isFullHealth)
             {
